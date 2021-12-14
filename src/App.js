de2ca3914412c923d42b36fcd4ccd4aee7813065
VinaySehwag14 <vinaysehwag14@gmail.com>
2021-12-14 17:02:27 +0530

    FINAL PUSH

--- src/App.js
+++ src/App.js
@@ -3,18 +3,35 @@ import "./App.css";
 import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
 import axios from "axios";
+import PunkList from "./components/PunkList";
+import Main from "./components/Main";
 
 const App = () => {
-  // const [state, setstate] = useState(initialState);
+  const [punkListData, setPunkListData] = useState([]);
+  const [selectedPunk, setSelectedPunk] = useState(0);
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0xD4B3F97E914ABB4FB7Fad21d935d73082CE10dCc&order_direction=asc"
+      );
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
+    return getMyNfts();
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <CollectionCard
-        id={0}
-        name={"Bandana Punk"}
-        traits={[{ value: 7 }]}
-        image="https://nftlabs.mypinata.cloud/ipfs/bafkreiasgnibw32isqfyjnv5lhj4wxzp6izayculjlo4ampmmfla3j4tj4"
-      />
+      {punkListData.length > 0 && (
+        <>
+          <Main punkListData={punkListData} selectedPunk={selectedPunk} />
+          <PunkList
+            punkListData={punkListData}
+            setSelectedPunk={setSelectedPunk}
+          />
+        </>
+      )}
     </div>
   );
 };
